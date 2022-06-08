712cd30fd933fd8e045848e1771022434feb8309
yun10002 <yjin4137@gmail.com>
2022-06-08 17:41:36 +0900

    20220608 Caliburn 마무리, Bogus 사용해보기

---
+++ portfolio/WpfPortfolio/DummyDataApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DummyDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) { 
+                var Rooms = new[] { "DINNING", "LIVING", "BATH", "BED" }; //부엌, 거실, 욕실, 침실
+
+                var sensorDummy = new Faker<SensorInfo>()
+                    .RuleFor(r => r.DevId, f => f.PickRandom(Rooms))
+                    .RuleFor(r => r.CurrTime, f => f.Date.Past(0)) //0 넣으면 현재
+                    .RuleFor(r => r.Temp, f => f.Random.Float(19.0f, 30.9f))
+                    .RuleFor(r => r.Humid, f => f.Random.Float(40.0f, 63.9f));
+
+                var currValue = sensorDummy.Generate();
+
+                Console.WriteLine(JsonConvert.SerializeObject(currValue, Formatting.Indented));
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
